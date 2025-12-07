d0c22f8deb710833d76ff00e26a7883da9a8271b
515m <104305611+515m@users.noreply.github.com>
2025-12-07 13:46:40 +0000

    blg
    Deltd clipboard

---
+++ post-v1.1.1.js
@@ -0,0 +1,33 @@
+const navbarMenu = document.getElementById("menu-p");
+const burgerMenu = document.getElementById("burger-p");
+const headerMenu = document.getElementById("header-p");
+
+// Toggle Navbar Menu on Burger Click
+if (burgerMenu && navbarMenu) {
+   burgerMenu.addEventListener("click", () => {
+      burgerMenu.classList.toggle("is-active");
+      navbarMenu.classList.toggle("is-active");
+   });
+}
+
+// Closed Navbar Menu on Links Click
+document.querySelectorAll(".menu-link-post").forEach((link) => {
+   link.addEventListener("click", () => {
+      burgerMenu.classList.remove("is-active");
+      navbarMenu.classList.remove("is-active");
+   });
+});
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener("resize", () => {
+   if (window.innerWidth >= 769) {
+      if (navbarMenu.classList.contains("is-active")) {
+         navbarMenu.classList.remove("is-active");
+      }
+   }
+});
+
+
+const d = new Date();
+let copyrightyear = d.getFullYear();
+document.getElementById("copyright").innerText = copyrightyear;
